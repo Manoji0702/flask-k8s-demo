9494e2249756e6a7d6bad1515d94e23affab7b6b
Manoj0207 <Manojpecit@gmail.com>
2025-11-05 13:31:27 +0530

    Fix terraform code2

--- terraform/varaibles.tf
+++ terraform/varaibles.tf
@@ -1,9 +1,11 @@
 variable "image" {
-  description = "manoj0207/flask-k8s-demo"
+  description = "Docker image to deploy"
   type        = string
+  default     = "manoj0207/flask-k8s-demo"
 }
 
 variable "kubeconfig_path" {
-  description = "D:\Kube_Config\admin.conf"
+  description = "Path to kubeconfig file"
   type        = string
+  default     = "D:\\Kube_Config\\admin.conf"
 }
