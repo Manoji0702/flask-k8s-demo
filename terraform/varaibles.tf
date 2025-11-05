9c6d602a2ca9e0db599ac4331058638fc2ab8420
Manoj0207 <Manojpecit@gmail.com>
2025-11-05 13:29:32 +0530

    Fix terraform code1

---
+++ terraform/varaibles.tf
@@ -0,0 +1,9 @@
+variable "image" {
+  description = "manoj0207/flask-k8s-demo"
+  type        = string
+}
+
+variable "kubeconfig_path" {
+  description = "D:\Kube_Config\admin.conf"
+  type        = string
+}
