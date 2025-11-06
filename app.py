50cbeb8c029e606248c254ff8d7b99b96292976a
Manoj0207 <Manojpecit@gmail.com>
2025-11-06 10:34:23 +0530

    newaddtion4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Kubernetes-test3!</h1>"
+    return "<h1>Hello from Kubernetes-test4!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
